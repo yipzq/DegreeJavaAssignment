2ed38e9a32bbde75a08598252d271e23c11deceb
yipzq <yipzhengqing36@gmail.com>
2024-01-02 11:04:27 +0800

    test

--- src/javaassignment/JavaAssignment.java
+++ src/javaassignment/JavaAssignment.java
@@ -16,7 +16,7 @@ public class JavaAssignment {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");
-        System.out.println("joshua noob");
+        System.out.println("joshua nssssoob");
     }
     
 }
